bd3b1e0636f7451954668771680bc753601b6c00
Adriano <adrianomarque135@gmail.com>
2025-02-06 23:34:09 -0300

    exercicio resolvido

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test('Deve renderizar os comentarios corretamente', () => {
+        render(<PostComment />)
+
+        fireEvent.change(screen.getByTestId('textarea'), {
+            target: {
+                value: 'primeiro comentario para teste'
+            }
+        })
+        fireEvent.click(screen.getByTestId('botao-enviar'))
+
+        fireEvent.change(screen.getByTestId('textarea'), {
+            target: {
+                value: 'segundo comentario para teste'
+            }
+        })
+        fireEvent.click(screen.getByTestId('botao-enviar'))
+
+        // eslint-disable-next-line testing-library/no-node-access
+        expect(screen.getByTestId('lista-comentarios').children).toHaveLength(2)
+    })
 });
